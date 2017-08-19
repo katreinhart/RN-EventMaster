c1f8bdb68baa77123f4d46d3f67c7ffe4a32b3d9
Kat Reinhart <kat.reinhart@gmail.com>
2017-08-19 10:51:28 -0700

    Initial Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Main from './src/main';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Main />
+      </View>
+    );
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,26 @@
+//  https://www.eventbriteapi.com/v3/users/me/?token=SESXYS4X3FJ5LHZRWGKQ
+
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text>Event Expert</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
